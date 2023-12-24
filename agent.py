562e58f20b6c32613ad344f9651ed7b3bcc9c0bf
joeyadelman <adelmanjoey@gmail.com>
2023-12-23 18:07:49 -0600

    Can use old model now when running

--- agent.py
+++ agent.py
@@ -1,6 +1,8 @@
 import torch
 import random
 import numpy as np
+import os
+import sys
 from collections import deque
 from agent_snake_game import SnakeGameAI, Direction, Point
 from model import Linear_QNet, QTrainer
@@ -10,15 +12,22 @@ MAX_MEMORY = 100_000
 BATCH_SIZE = 1000
 LR = 0.001
 
+ARGS = sys.argv
+
 
 class Agent():
 
     def __init__(self):
+        self.use_old = False
         self.number_of_games = 0
         self.epsilon = 0  # control the randomness
         self.gamma = 0.9  # discount rate
         self.memory = deque(maxlen=MAX_MEMORY)  # popleft() when full
         self.model = Linear_QNet(11, 256, 3)
+        if os.path.exists("./model/model.pth") and len(ARGS) == 2 and str(ARGS[1]) == 'model':
+            self.model.load_state_dict(torch.load('./model/model.pth'))
+            self.model.eval()
+            self.use_old = True
         self.trainer = QTrainer(self.model, lr=LR, gamma=self.gamma)
 
     def get_state(self, game):
@@ -83,11 +92,17 @@ class Agent():
 
     def get_action(self, state):
         # random moves: tradeoff of exploration / exploitation
-        self.epsilon = 80 - self.number_of_games
         final_move = [0, 0, 0]
-        if random.randint(0, 200) < self.epsilon:
-            move = random.randint(0, 2)
-            final_move[move] = 1
+        if self.use_old == False:
+            self.epsilon = 80 - self.number_of_games
+            if random.randint(0, 200) < self.epsilon:
+                move = random.randint(0, 2)
+                final_move[move] = 1
+            else:
+                state0 = torch.tensor(state, dtype=torch.float)
+                prediction = self.model(state0)
+                move = torch.argmax(prediction).item()
+                final_move[move] = 1
         else:
             state0 = torch.tensor(state, dtype=torch.float)
             prediction = self.model(state0)
@@ -101,7 +116,7 @@ def train():
     plot_scores = []
     plot_mean_scores = []
     total_score = 0
-    record = 0
+    record = 0 
     agent = Agent()
     game = SnakeGameAI()
     while True:
--- readme.md
+++ readme.md
@@ -1,7 +1,26 @@
 # Create Vitual Environment
 
-'''python
+```python
 python3 -m venv venv
 source env/bin/activate
 pip install -r requirements.txt
-'''
+```
+
+# Start user snake game
+
+```python
+python3 user_snake_game.py
+```
+
+# Start trainer
+
+```python
+python3 agent.py
+```
+
+
+# If model created and you want to use the previous model
+
+```python
+python3 agent.py model
+```
